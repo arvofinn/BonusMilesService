94a0e4c3f7fdc27e3964d1b7708db1ed90f8c8c3
Arvo <arvofinland@gmail.com>
2022-12-26 22:17:10 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        int ticket = 10_000;
+        int rub = 20;
+
+        int mile;
+
+        System.out.println(mile = ticket / rub);
     }
 }
